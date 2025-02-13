c16c0d58c02bbe4ba414a18071dadd7c1e858e36
Ethan Niser <ethan@niser.com>
2025-02-13 01:22:18 -0500

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,28 @@
-import { Effect } from "effect";
-import { create_notion_client, get_notion_published } from "./notion";
-import {
-  create_redis_client,
-  get_saved_published_videos,
-  save_video,
-} from "./redis";
-import { create_discord_client, send_discord_message } from "./discord";
-import { get_env_variable } from "./env";
+import { Config, Effect, Predicate } from "effect";
+import * as Notion from "./notion.js";
+import * as Redis from "./redis.js";
+import * as Discord from "./discord.js";
 
-export const get_videos_not_sent = Effect.gen(function* () {
-  const notion_db_id = yield* get_env_variable("NOTION_DATABASE_ID");
-  const discord_channel_id = yield* get_env_variable("DISCORD_CHANNEL_ID");
+export const main = Effect.gen(function* () {
+  const notionDbId = yield* Config.string("NOTION_DATABASE_ID");
+  const discordChannelId = yield* Config.string("DISCORD_CHANNEL_ID");
 
-  const notion_client = yield* create_notion_client;
-  const redis_client = yield* create_redis_client;
-  const discord_client = yield* create_discord_client;
+  const notionVideos = yield* Notion.getPublished(notionDbId);
+  const redisVideos = yield* Redis.getPublishedSavedVideos;
 
-  const notion_videos = yield* get_notion_published(
-    notion_client,
-    notion_db_id
-  );
-  const redis_videos = yield* get_saved_published_videos(redis_client);
+  for (const video of notionVideos.results) {
+    const id = video.id;
+    const title = video.properties.Title.title[0].plain_text;
 
-  const has_key = <T extends object>(obj: T, key: keyof T): boolean => {
-    return Object.prototype.hasOwnProperty.call(obj, key);
-  };
-
-  for (const notion_video of notion_videos.results) {
-    const video_id = notion_video.id;
-    const video_properties = (notion_video as any).properties;
-    const video_title = video_properties.Title.title[0].plain_text as string;
-
-    if (has_key(redis_videos, video_id)) {
-      console.log(`${video_id} in DB`);
+    if (Predicate.hasProperty(redisVideos, id)) {
+      yield* Effect.logInfo(`Video ${id} already in DB`);
     } else {
-      console.log(`need to add ${video_id}`);
-      yield* save_video(redis_client, video_id, { title: video_title });
-      yield* send_discord_message(
-        discord_client,
-        discord_channel_id,
-        `VIDEO WAS JUST PUBLISHED: ${video_title}`
+      yield* Effect.logInfo(`Video ${id} not in DB`);
+      yield* Redis.saveVideo(id, { title });
+      yield* Discord.sendMessage(
+        discordChannelId,
+        `VIDEO WAS JUST PUBLISHED: ${title}`
       );
     }
   }
-});
+}).pipe(Effect.withLogSpan("main"));
--- src/redis.ts
+++ src/redis.ts
@@ -1,70 +1,99 @@
-import { Effect } from "effect";
+import { Config, Context, Data, Effect, Layer, Schema } from "effect";
 import { createClient } from "redis";
-import type { RedisClientType } from "redis";
-import { get_env_variable } from "./env";
 
-type VideoSchema = {
-  title: string;
-};
+export class RedisError extends Data.TaggedError("RedisError")<{
+  cause?: unknown;
+  message?: string;
+}> {}
 
-const ALL_VIDEOS_ID = "videos:all";
+interface RedisImpl {
+  use: <T>(
+    fn: (client: ReturnType<typeof createClient>) => T
+  ) => Effect.Effect<Awaited<T>, RedisError, never>;
+}
+export class Redis extends Context.Tag("Redis")<Redis, RedisImpl>() {}
 
-export const create_redis_client = Effect.promise(async () => {
-  const redis_url = Effect.runSync(get_env_variable("REDIS_URL"));
-
-  const client = createClient({
-    url: redis_url,
-  }) as RedisClientType;
-
-  await client.connect();
-
-  return client;
-});
-
-export const save_video = (
-  client: RedisClientType,
-  video_id: string,
-  video_data: VideoSchema
-) =>
-  Effect.promise(async () => {
-    await client.hSet(ALL_VIDEOS_ID, video_id, JSON.stringify(video_data));
-    console.log(`Saved to redis: ${video_id}`);
+export const make = (options?: Parameters<typeof createClient>[0]) =>
+  Effect.gen(function* () {
+    const client = yield* Effect.acquireRelease(
+      Effect.tryPromise({
+        try: () => createClient(options).connect(),
+        catch: (e) => new RedisError({ cause: e }),
+      }),
+      (client) => Effect.promise(() => client.quit())
+    );
+    return Redis.of({
+      use: (fn) =>
+        Effect.gen(function* () {
+          const result = yield* Effect.try({
+            try: () => fn(client),
+            catch: (e) =>
+              new RedisError({
+                cause: e,
+                message: "Syncronous error in `Redis.use`",
+              }),
+          });
+          if (result instanceof Promise) {
+            return yield* Effect.tryPromise({
+              try: () => result,
+              catch: (e) =>
+                new RedisError({
+                  cause: e,
+                  message: "Asyncronous error in `Redis.use`",
+                }),
+            });
+          } else {
+            return result;
+          }
+        }),
+    });
   });
 
-export const get_saved_published_videos = (client: RedisClientType) =>
-  Effect.promise(async () => {
-    const all_videos = await client.hGetAll(ALL_VIDEOS_ID);
+export const layer = (options?: Parameters<typeof createClient>[0]) =>
+  Layer.scoped(Redis, make(options));
 
-    let videos: Record<string, VideoSchema> = {};
+export const fromEnv = Layer.scoped(
+  Redis,
+  Effect.gen(function* () {
+    const url = yield* Config.string("REDIS_URL");
+    return yield* make({ url });
+  })
+);
 
-    for (const [video_id, video_data] of Object.entries(all_videos)) {
-      const data = JSON.parse(video_data) as VideoSchema;
-      videos[video_id] = data;
-      console.log(`Video ID: ${video_id}, Data: ${data.title}`);
-    }
+const VideoData = Schema.parseJson(
+  Schema.Struct({
+    title: Schema.String,
+  })
+);
+type VideoData = Schema.Schema.Type<typeof VideoData>;
 
-    return videos;
-  });
+export const saveVideo = (id: string, data: VideoData) =>
+  Effect.gen(function* () {
+    const redis = yield* Redis;
+    const encodedData = yield* Schema.encode(VideoData)(data);
+    yield* redis.use((client) => client.hSet("videos", id, encodedData));
+    yield* Effect.logInfo(`Saved to redis: ${id}`);
+  }).pipe(Effect.withLogSpan("saveVideo"));
 
-export const clear_redis_db = (client: RedisClientType) =>
-  Effect.promise(async () => {
-    await client.flushAll();
-    console.log("Redis database cleared");
-  });
+export const getPublishedSavedVideos = Effect.gen(function* () {
+  const redis = yield* Redis;
+  const rawVideos = yield* redis.use((client) => client.hGetAll("videos"));
+  const parsedVideos = yield* Schema.decode(
+    Schema.Record({
+      key: Schema.String,
+      value: VideoData,
+    })
+  )(rawVideos);
 
-export const save_dummy_videos = (client: RedisClientType) =>
-  Effect.promise(async () => {
-    const videos: Record<string, VideoSchema> = {
-      "video:1": {
-        title: "first test",
-      },
-      "video:2": {
-        title: "second test",
-      },
-    };
+  for (const [id, data] of Object.entries(parsedVideos)) {
+    yield* Effect.logInfo(`Recieved: Video ID: ${id}, Data: ${data.title}`);
+  }
 
-    for (const [video_id, video_data] of Object.entries(videos)) {
-      await client.hSet(ALL_VIDEOS_ID, video_id, JSON.stringify(video_data));
-      console.log(`Saved to redis: ${video_id}`);
-    }
-  });
+  return parsedVideos;
+}).pipe(Effect.withLogSpan("getPublishedSavedVideos"));
+
+export const clearRedisDb = Effect.gen(function* () {
+  const redis = yield* Redis;
+  yield* redis.use((client) => client.flushAll());
+  yield* Effect.logInfo("Redis database cleared");
+}).pipe(Effect.withLogSpan("clearRedisDb"));
